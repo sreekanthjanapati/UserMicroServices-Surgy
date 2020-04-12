85ca1718baced24e23c28fa4ad3a364077b6ece6
sreekanthjanapati <sreekanth@test.com>
2020-04-13 01:50:06 +0530

    properties changes

--- src/main/java/com/microservices/controllers/HelloController.java
+++ src/main/java/com/microservices/controllers/HelloController.java
@@ -1,5 +1,7 @@
 package com.microservices.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("v1/hello")
 public class HelloController {
 	
+	@Autowired
+	private Environment env;
+	
 	@GetMapping
 	public String getHello() {
 		
-		return "User micro service running succefully";
+		return "User micro service running succefully :: "+env.getProperty("server.port");
 	}
 
 }
